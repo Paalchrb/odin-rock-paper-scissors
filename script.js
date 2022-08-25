2df9ba403f6712f83e6737633c0b36246c21e4be
pal-christianby <pal.christian.by@ntb.no>
2022-08-25 10:13:23 +0200

    Add pseudocode for program

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("connected");
+// Store computer choices in variable
+// To play a round:
+  // Randomly pick a computer choice
+  // Get user input
+  // Make sure input is valid "Rock", "Paper" or "Scissors"
+  // Decide who wins
+  // Let user know the result
+
+// To play a game of 5:
+  // Use a iterator to play 5 rounds
+  // Call the round function for each iteration
+  // Update the score of the game winner
+  // Announce final score after 5 games
